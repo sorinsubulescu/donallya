5043c2ba46609b8d800f1bcaea436d349f6caad7
Sorin <sorinsubulescu@gmail.com>
2013-04-07 16:26:11 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Donallya::Application.config.secret_token = '1077010c435eae921c12672715470f248073d32b0fba041464a5348d74c9b85873b578245214d14f518e953399699f393d08a4283a038353857315e4a8a33e83'
